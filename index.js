fe7f8a0b95df891a1ca7121eeaba2fc2918af0d1
Fahim-05 <fahim45147@gmail.com>
2022-11-08 18:47:43 +0600

    env gitignore done

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9h6j3eg.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
