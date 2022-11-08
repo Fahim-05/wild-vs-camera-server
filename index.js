e2971f54e40d2fedeb0b61e1676e30998dd26287
Fahim-05 <fahim45147@gmail.com>
2022-11-08 18:55:54 +0600

    function added

--- index.js
+++ index.js
@@ -14,11 +14,17 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9h6j3eg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+
+    }
+    finally{
+        
+    }
+
+}
+run().catch(error => console.error(error));
 
 
 
