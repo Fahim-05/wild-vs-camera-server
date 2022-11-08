bcfa1a1947502f082b38b8514e7ec71ae9e79e68
Fahim-05 <fahim45147@gmail.com>
2022-11-08 18:35:41 +0600

    basic server create done

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 5000;
 
-// middlewares
+// Middleware
 app.use(cots());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Wild-Photographer server is running!')
+})
+
+app.listen(port, () => {
+    console.log(`Wild-Photographer server is running on ${port}`);
+})
