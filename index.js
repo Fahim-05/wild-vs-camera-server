f4d616f6897228b5a62b255446290c47a4f6bb79
Fahim-05 <fahim45147@gmail.com>
2022-11-10 20:34:40 +0600

    all done

--- index.js
+++ index.js
@@ -19,35 +19,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        res.status(401).send({message: 'unauthorized access'})
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded){
-       if(err) {
-        res.status(401).send({message: 'unauthorized access'})
-       } 
-       req.decoded = decoded;
-       next();
-    })
-}
-
 
 async function run() {
     try {
         const serviceCollection = client.db('Wild_Photograph').collection('services')
         const reviewCollection = client.db('Wild_Photograph').collection('review')
-
-
-        // JWT api
-        app.post('/jwt', (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            res.send({ token });
-        })
-
 
 
         app.get('/services', async (req, res) => {
@@ -95,13 +71,9 @@ async function run() {
         });
 
 
-        app.get('/review', verifyJWT, async (req, res) => {
+        app.get('/review',  async (req, res) => {
 
-            const decoded = req.decoded;
-            console.log(decoded);
-            if(decoded.email !== req.query.email){
-                res.status(403).send({message: 'unauthorized access'})
-            }
+            
 
             let query = {};
             if (req.query.email) {
